6a806302782caaaaa42ddcfd5f983ccfcff20b7a
BhagyashreeSethiya <bhagyashreesethiya@gmail.com>
2026-03-28 08:52:00 +0000

    Initial commit - OSS audit project

---
+++ scripts/user_interaction.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "========== User Interaction =========="
+
+echo "Enter your name:"
+read name
+
+echo "Hello, $name!"
+
+echo "Who is your inspiration?"
+read inspiration
+
+echo "$inspiration was a great man."
+
+echo "======================================"
